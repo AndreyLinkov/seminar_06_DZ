d3d0f566470617836974bdfa425606092d612d8a
Andrey Linkov <linkov.developer@inbox.ru>
2023-12-05 00:33:09 +0300

    task_03

---
+++ task_03/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.Clear();
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumMatrix(int[,] matrix)
+{
+    int index = 0;
+    int minSum = int.MaxValue;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        if (sum < minSum)
+        {
+            minSum = sum;
+            index = i;
+        }
+    }
+    return index;
+}
+
+int[,] matrix = new int[5, 3];
+
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+int minimal = SumMatrix(matrix);
+Console.WriteLine($"Минимальная сумма элементов находится на {minimal+1} строке .");
